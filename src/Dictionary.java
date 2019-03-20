64cd3ed9fe5673f9cd1c6baaf88c1779c3575502
lamecode <44145440+lamecode@users.noreply.github.com>
2019-03-20 13:08:01 +0200

    adding input file and working out

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Dictionary {
+    private static final String INPUT_FILE = "dict_processed.txt";
+
     public static void main(String[] args) {
+        try {
+            BufferedReader brand_new = new BufferedReader(new FileReader(INPUT_FILE));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         Scanner Lab = new Scanner(System.in);
         String word = Lab.nextLine();
     }
