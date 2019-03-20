2e9f9671c12deb491454fc840c9f297d7c2ca17b
Ammar <s95ammar@gmail.com>
2019-03-20 14:13:44 +0200

    added sentence processing

--- src/dictionary/Dictionary.java
+++ src/dictionary/Dictionary.java
@@ -4,8 +4,16 @@ import java.util.HashMap;
 
 public class Dictionary extends HashMap<String, String> {
 
-    public void define(String word) {
-        System.out.println(get(word));
+    public void define(String sentence) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (get(words[i]) != null){
+                System.out.println(get(words[i]) + "\n");
+            } else {
+                System.out.println(words[i] + ": Not found\n");
+            }
+
+        }
 
     }
 
