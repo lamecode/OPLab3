51e806279bb9f5dbbb7543431cf44baccf50d7a9
lamecode <44145440+lamecode@users.noreply.github.com>
2019-03-20 11:13:29 +0200

    creating source

---
+++ src/Dictionary.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Dictionary {
+    public static void main(String[] args) {
+        Scanner Lab = new Scanner(System.in);
+        String word = Lab.nextLine();
+    }
+}
