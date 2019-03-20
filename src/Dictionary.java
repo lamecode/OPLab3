84edba0ac7165629702411e9189d63eef89aaf25
Ammar <s95ammar@gmail.com>
2019-03-20 11:33:30 +0200

    first commit in new branch

--- src/Dictionary.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class Dictionary {
-    public static void main(String[] args) {
-        Scanner Lab = new Scanner(System.in);
-        String word = Lab.nextLine();
-    }
-}
