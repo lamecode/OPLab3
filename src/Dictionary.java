f5f8501d6e9a5b193d095d821da56e46455d21e9
lamecode <44145440+lamecode@users.noreply.github.com>
2019-03-27 09:52:16 +0200

    getting rid of some classes, modyfying HAshTable class, extending Dictionary class

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,14 +1,34 @@
-import java.util.HashMap;
+public class Dictionary extends HAshTable {
 
-public class Dictionary extends HashMap<String, String> {
-    public void AddDefinition(String sentence) {
-        String[] words = sentence.split(" ");
+    public void AddDefinition(String text) {
+        String[] words = text.split(" ");
         for (int i = 0; i < words.length; i++) {
-            if (get(words[i]) == null) {
-                System.out.println(words[i] + ": not found" + "\n");
+            if (get(words[i]) != null) {
+                System.out.println(words[i] + ": " + get(words[i]) + "\n");
             } else {
-                System.out.println(get(words[i]) + "\n");
+                System.out.println(words[i] + ": Not found\n");
             }
         }
     }
+
+
+
+/*
+    @Override
+    public String toString() { //view only the keys
+        String toStr = "{\n";
+        for (int i = 0; i < CAPACITY; i++) {
+            String list = "(" + table[i].key;
+            Node last = table[i];
+            while (last.next != null) {
+                last = last.next;
+                list += " |-> " + last.key;
+            }
+            list += ")";
+            toStr += list + "\n";
+        }
+        toStr += "}";
+        return toStr;
+    }
+*/
 }
